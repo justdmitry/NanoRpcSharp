9c7c7e5abbfb6999ec92c37595376c219cf2a846
Dmitry Popov <dmitry@parsec-club.ru>
2019-04-09 12:38:57 +0300

    codestyle: whitespace fix

--- NanoRpcSharp.Tests/Messages/AccountsCreateTests.cs
+++ NanoRpcSharp.Tests/Messages/AccountsCreateTests.cs
@@ -14,8 +14,8 @@
 
             var validJson = @"
 {  
-  ""action"": ""accounts_create"",  
-  ""wallet"": ""000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F"",  
+  ""action"": ""accounts_create"",
+  ""wallet"": ""000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F"",
   ""count"": 2,
   ""work"": ""false""
 }
@@ -30,15 +30,15 @@
             var validJson = @"
 {  
   ""accounts"": [
-     ""xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000"",   
+     ""xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000"",
      ""xrb_1e5aqegc1jb7qe964u4adzmcezyo6o146zb8hm6dft8tkp79za3s00000000""
-  ]  
+  ]
 }
 ";
             var obj = NanoRpcClient.Deserialize<AccountsCreate>(validJson);
 
             Assert.Equal(
-                new Account[] 
+                new Account[]
                 {
                     "xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000",
                     "xrb_1e5aqegc1jb7qe964u4adzmcezyo6o146zb8hm6dft8tkp79za3s00000000",
